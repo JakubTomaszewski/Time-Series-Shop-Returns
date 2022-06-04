e98d9266766605c7b1054938a2a5514d317a0f81
Gimli037 <wiktor.kulesza037@gmail.com>
2022-06-04 20:18:09 +0200

    Feat: Added 2 endpoint to use regression

--- deploy/microservice-backend/request.py
+++ deploy/microservice-backend/request.py
@@ -1,7 +1,7 @@
 import requests
 
-url = 'http://localhost:8020/api'
-response = requests.post(url, json={'day': 10, 'month': 2, 'year': 2020, 'path': 'random_forest_model.joblib'})
+url = 'http://localhost:8020/api/regression'
+response = requests.post(url, json={'day': 10, 'month': 2, 'year': 2020})
 
 if response.status_code == 200:
     print(response.json())
--- deploy/microservice-backend/server.py
+++ deploy/microservice-backend/server.py
@@ -7,18 +7,20 @@ from flask_cors import CORS
 from model_utils import CycleTransformer, load_model_pipeline
 
 NUM_DAYS = 14
-MODEL_PATH = "../../models/"
+RANDOM_FOREST_MODEL_PATH = "../../models/random_forest_model.joblib"
+LINEAR_REGRESSION_MODEL_PATH = "../../models/linear_regression_model.joblib"
 
 app = Flask(__name__)
 CORS(app)
 
+forest_model = load_model_pipeline(RANDOM_FOREST_MODEL_PATH)
+# regression_model = load_model_pipeline(LINEAR_REGRESSION_MODEL_PATH)
 
-@app.route('/api', methods=['POST'])
-def predict():
+
+@app.route('/api/forest', methods=['POST'])
+def predict_forest():
     # Get the data from the POST request.
     data = request.get_json(force=True)
-    path = data['path']
-    model = load_model_pipeline(MODEL_PATH+path)
     # Crete 14 dates from date passed by user
     base = datetime.datetime(data['year'], data['month'], data['day'])
     date_list = [base + datetime.timedelta(days=x) for x in range(14)]
@@ -26,7 +28,30 @@ def predict():
         [[date.day, date.month, date.year] for date in date_list])
 
     # Make prediction using model loaded from disk
-    predictions = model.predict(date_list_prepared)
+    predictions = forest_model.predict(date_list_prepared)
+
+    # Jsonify predictions
+    js = {}
+    for date, prediction in zip(date_list, predictions):
+        js[date.strftime("%m/%d/%Y")] = prediction
+
+    json_object = json.dumps(js, indent=4)
+    # return json response
+    return json_object
+
+
+@app.route('/api/regression', methods=['POST'])
+def predict_regression():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+    # Crete 14 dates from date passed by user
+    base = datetime.datetime(data['year'], data['month'], data['day'])
+    date_list = [base + datetime.timedelta(days=x) for x in range(14)]
+    date_list_prepared = np.array(
+        [[date.day, date.month, date.year] for date in date_list])
+
+    # Make prediction using model loaded from disk
+    predictions = forest_model.predict(date_list_prepared)
 
     # Jsonify predictions
     js = {}
