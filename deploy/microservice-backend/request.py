b80ed9276b34234ccf7aa8426a4d9e0dea6498d5
Jakub Tomaszewski <Kuba@MacBook-Pro.local>
2022-06-28 18:25:37 +0200

    Remove redundant file

--- deploy/microservice-backend/request.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://localhost:8020/api/regression'
-response = requests.post(url, json={'day': 10, 'month': 2, 'year': 2020})
-
-if response.status_code == 200:
-    print(response.json())
